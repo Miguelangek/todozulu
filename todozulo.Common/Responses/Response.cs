7da8a41921436d3349249fa38bed15d2101c3aab
migue <miguelescobar225599@corre.itm.edu.co>
2021-08-16 15:21:10 -0500

    Add entity

---
+++ todozulo.Common/Responses/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todozulo.Common.Responses
+{
+     public class Response
+    {
+        public Boolean ItSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+    }
+}
